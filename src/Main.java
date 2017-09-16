96f0b40ad7c2c5ccc8a65763eec1040141584e52
Srikar-Velineni <31933987+Srikar-Velineni@users.noreply.github.com>
2017-09-17 01:38:26 +0530

    final3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//You may test that your code works find here
+		Student s= new Student();
+                StudentArrayOperation ao= new StudentArrayOperation();
+                StudentGroup sg= new StudentGroup();
+//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
 	}
